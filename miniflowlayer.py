c1d260200d31142f7cadfcfd712e6cab114ba47c
cameronwp <cameron.w.pittman@gmail.com>
2016-10-26 12:45:08 -0500

    adds working example of SGD

--- miniflowlayer.py
+++ miniflowlayer.py
@@ -1,26 +1,60 @@
 import numpy as np
+import random
 
 class Layer:
+    """
+    Base class for layers in the network.
+
+    Arguments:
+
+        `inbound_layers`: A list of layers with edges into this layer.
+    """
     def __init__(self, inbound_layers=[]):
+        """
+        Layer's constructor (runs when the object is instantiated). Sets
+        properties that all layers need.
+        """
+        # A list of layers with edges into this layer.
         self.inbound_layers = inbound_layers
+        # The eventual value of this layer. Set by running
+        # the forward() method.
         self.value = None
+        # A list of layers that this layer outputs to.
         self.outbound_layers = []
         # New property! Keys are the inputs to this layer and
         # their values are the partials of this layer with
         # respect to that input.
         self.gradients = {}
+        # Sets this layer as an outbound layer for all of
+        # this layer's inputs.
         for layer in inbound_layers:
             layer.outbound_layers.append(self)
 
     def forward():
+        """
+        Every layer that uses this class as a base class will
+        need to define its own `forward` method.
+        """
         raise NotImplementedError
 
     def backward():
+        """
+        Every layer that uses this class as a base class will
+        need to define its own `backward` method.
+        """
         raise NotImplementedError
 
 
 class Input(Layer):
+    """
+    A generic input into the network.
+    """
     def __init__(self):
+        # The base class constructor has to run to set all
+        # the properties here.
+        #
+        # The most important property on an Input is value.
+        # self.value is set during `topological_sort` later.
         Layer.__init__(self)
 
     def forward(self):
@@ -28,28 +62,42 @@ class Input(Layer):
         pass
 
     def backward(self):
-        # An Input layer has no inputs so the gradient starts
-        # at zero.
+        # An Input layer has no inputs so the gradient (derivative)
+        # is zero.
+        # The key, `self`, is reference to this object.
         self.gradients = {self: 0}
+        # Weights and bias may be inputs, so you need to sum
+        # the gradient from output gradients.
         for n in self.outbound_layers:
             self.gradients[self] += n.gradients[self]
 
 
 class Linear(Layer):
+    """
+    Represents a layer that performs a linear transform.
+    """
     def __init__(self, inbound_layer, weights, bias):
+        # The base class (Layer) constructor. Weights and bias
+        # are treated like inbound layers.
         Layer.__init__(self, [inbound_layer, weights, bias])
 
     def forward(self):
+        """
+        Performs the math behind a linear transform.
+        """
         inputs = self.inbound_layers[0].value
         weights = self.inbound_layers[1].value
         bias = self.inbound_layers[2].value
         self.value = np.dot(inputs, weights) + bias
 
     def backward(self):
+        """
+        Calculates the gradient based on the output values.
+        """
         # Initialize a partial for each of the inbound_layers.
         self.gradients = {n: np.zeros_like(n.value) for n in self.inbound_layers}
         # Cycle through the outputs. The gradient will change depending
-        # on each output.
+        # on each output, so the gradients are summed over all outputs.
         for n in self.outbound_layers:
             # Get the partial of the cost with respect to this layer.
             grad_cost = n.gradients[self]
@@ -62,7 +110,11 @@ class Linear(Layer):
 
 
 class Sigmoid(Layer):
+    """
+    Represents a layer that performs the sigmoid activation function.
+    """
     def __init__(self, layer):
+        # The base class constructor.
         Layer.__init__(self, [layer])
 
     def _sigmoid(self, x):
@@ -75,12 +127,20 @@ class Sigmoid(Layer):
         return 1. / (1. + np.exp(-x))
 
     def forward(self):
+        """
+        Perform the sigmoid function and set the value.
+        """
         input_value = self.inbound_layers[0].value
         self.value = self._sigmoid(input_value)
 
     def backward(self):
-        # Zero the gradients
+        """
+        Calculates the gradient using the derivative of
+        the sigmoid function.
+        """
+        # Initialize the gradients to 0.
         self.gradients = {n: np.zeros_like(n.value) for n in self.inbound_layers}
+        # Sum the gradients over all the outputs.
         for n in self.outbound_layers:
             grad_cost = n.gradients[self]
             sigmoid = self.value
@@ -95,8 +155,6 @@ class MSE(Layer):
 
         Arguments:
             `inbound_layer`: A layer with an activation function.
-            `ideal_output`: A numpy array.
-            `feed_dict`: The same feed_dict that sets the inputs.
         """
         # Call the base class' constructor.
         Layer.__init__(self, [inbound_layer])
@@ -113,15 +171,15 @@ class MSE(Layer):
         Calculates the mean squared error.
         """
         actual_output = self.inbound_layers[0].value
-        first = 1. / (2. * self.n_inputs)
+        first_term = 1. / (2. * self.n_inputs)
         norm = np.linalg.norm(self.ideal_output - actual_output)
-        self.value = first * np.square(norm)
+        self.value = first_term * np.square(norm)
 
     def backward(self):
         """
         Calculates the gradient of the cost.
         """
-        self.gradients[self.inbound_layers[0]] = 2 * self.inbound_layers[0].value
+        self.gradients[self.inbound_layers[0]] = -2 * self.inbound_layers[0].value
 
 
 # NOTE: assume y is a vector with values 0-9
@@ -217,11 +275,45 @@ def forward_and_backward(feed_dict, ideal_output):
     # Backward pass
     reversed_layers = sorted_layers[::-1] # see: https://docs.python.org/2.3/whatsnew/section-slices.html
 
-    # pass the cost to the first layer?
     for n in reversed_layers:
         n.backward()
 
     return sorted_layers
+
+
+def train_SGD(feed_dict, ideal_output, trainables=[], epochs=1, learning_rate=1e-2):
+    """
+    Performs a forward pass and a backward pass through a list of sorted Layers.
+
+    Arguments:
+
+        `feed_dict`: A dictionary where the key is a `Input` Layer and the value is the respective value feed to that Layer.
+        `ideal_output`: The correct output value for the last activation layer.
+        `trainables`: Inputs that need to be modified by SGD.
+        `epochs`: The number of times to train against all training inputs.
+        `learning_rate`: The step size for changes by each gradient.
+    """
+
+    sorted_layers = topological_sort(feed_dict, ideal_output)
+
+    for i in range(epochs):
+        # Forward pass
+        for n in sorted_layers:
+            n.forward()
+
+        # Backward pass
+        reversed_layers = sorted_layers[::-1] # see: https://docs.python.org/2.3/whatsnew/section-slices.html
+
+        for n in reversed_layers:
+            n.backward()
+
+        # Performs SGD
+        gradient_cost = [n.gradients[n] for n in trainables]
+        for n in range(len(trainables)):
+            trainables[n].value -= learning_rate * gradient_cost[n]
+        print('Epoch: ' + str(i) + ', Loss: ' + str(sorted_layers[-1].value))
+
+    return sorted_layers[-1].value
 
 
 # NOTE: This layer is just here to pass dummy gradients backwards for testing
--- test-sgd.py
+++ test-sgd.py
@@ -4,8 +4,8 @@ from miniflowlayer import *
 inputs, weights, bias = Input(), Input(), Input()
 
 x = np.array([[-1., -2.], [-1, -2]])
-w = np.array([[2., -3], [2., -3]])
-b = np.array([-3., -5])
+w = np.array([[random.random(), random.random()], [random.random(), random.random()]])
+b = np.array([random.random(), random.random()])
 ideal_output = np.array(
     [[1.23394576e-04, 9.82013790e-01],
     [1.23394576e-04, 9.82013790e-01]])
@@ -15,11 +15,5 @@ g = Sigmoid(f)
 cost = MSE(g)
 
 feed_dict = {inputs: x, weights: w, bias: b}
-gradiented_layers = forward_and_backward(feed_dict, ideal_output)
 
-"""
-what's the output?
-"""
-# print(gradiented_layers)
-for l in gradiented_layers:
-    print(l.gradients)
+train_SGD(feed_dict, ideal_output, [weights, bias], 40)
